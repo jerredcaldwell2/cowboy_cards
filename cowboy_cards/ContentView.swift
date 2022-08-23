da83bf62b18e826dfc8856c5071ac8d84961df46
jerredcaldwell <jerredc09@gmail.com>
2022-08-23 12:01:27 -0500

    Initial Commit

---
+++ cowboy_cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cowboy_cards
+//
+//  Created by Jerred Caldwell on 8/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cowboy_cards/cowboy_cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cowboy_cardsApp.swift
+//  cowboy_cards
+//
+//  Created by Jerred Caldwell on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct cowboy_cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
